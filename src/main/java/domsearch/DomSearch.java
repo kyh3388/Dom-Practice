e353e323458c35969bd8e7fc437b51e4e517cb97
kyh3388 <kimmm3388@naver.com>
2026-06-08 10:36:15 +0900

    dom-practice v1.0

--- src/main/java/domsearch/DomSearch.java
+++ src/main/java/domsearch/DomSearch.java
@@ -1,12 +1,7 @@
 package domsearch;
 
-import java.io.File;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 
 public class DomSearch {
